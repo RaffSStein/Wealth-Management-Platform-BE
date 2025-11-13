65000f4ebd0c5bb6df13cc136832e8db9ee52cf7
Raffaele Prince <raffaele.princee@gmail.com>
2025-11-13 23:03:17 +0100

    Add UserCreatedEventConsumer and EmailController with Kafka integration

---
+++ business-modules/email-service/email-core/src/main/java/raff/stein/email/controller/EmailController.java
@@ -0,0 +1,9 @@
+package raff.stein.email.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class EmailController {
+}
