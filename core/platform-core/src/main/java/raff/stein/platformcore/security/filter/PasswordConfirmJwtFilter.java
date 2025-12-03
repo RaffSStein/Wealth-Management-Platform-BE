4dbdceffb8a4f86b01c5489c1989b5d7e09f49d9
Raffaele Prince <raffaele.princee@gmail.com>
2025-12-03 09:16:36 +0100

    Upgrade to Spring Boot 4.0.0 and needed dependency versions

--- core/platform-core/src/main/java/raff/stein/platformcore/security/filter/PasswordConfirmJwtFilter.java
+++ core/platform-core/src/main/java/raff/stein/platformcore/security/filter/PasswordConfirmJwtFilter.java
@@ -1,6 +1,5 @@
 package raff.stein.platformcore.security.filter;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
@@ -13,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.io.InputStream;
